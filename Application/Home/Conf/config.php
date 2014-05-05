e395bb86f71a71b26f57ceae4a39c0a9af96ed4a
zhutaofen <575956381@qq.com>
2014-05-05 21:32:54 +0800

    origin

---
+++ Application/Home/Conf/config.php
@@ -0,0 +1,13 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+	'URL_MODEL'          => '2', //URL模式
+    'SESSION_AUTO_START' => true, //是否开启session
+    'DB_TYPE'=>'mysql',
+	'DB_HOST'=>'localhost',
+	'DB_PORT'=>'3306',
+	'DB_NAME'=>'weikucms',
+	'DB_USER'=>'root',
+	'DB_PWD'=>'root',
+	'DB_PREFIX'=>'tp_',
+);
